14f70eeef7d970967265f447abeeb787a2ca630c
emmanuel-gyateng <57065066+Emmanuel-Okyere@users.noreply.github.com>
2022-07-27 09:27:46 +0000

    Added 9 test cases

--- authentications/tests.py
+++ authentications/tests.py
@@ -88,6 +88,15 @@ class ChangeUserPassword(TestSetup):
         )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
+    def test_unauthenticated_users_can_not_change_paswword(self):
+        self.client.force_authenticate(user=None)
+        response = self.client.patch(
+            self.change_password_url,
+            {"old_password": "admin1234", "new_password": "newPassword123"},
+            format="json",
+        )
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
 
 class TestManagerRegisterAdmin(TestSetup):
     """Api test for manager registration of users"""
