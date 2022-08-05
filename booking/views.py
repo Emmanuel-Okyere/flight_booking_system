551d84a1008971f55dc08ce3e54468fc8cfb2ca1
emmanuel-gyateng <57065066+Emmanuel-Okyere@users.noreply.github.com>
2022-08-05 20:50:50 +0000

    Deduct seats available upon every booking

--- booking/views.py
+++ booking/views.py
@@ -1,40 +1,49 @@
-from django.shortcuts import render
 from rest_framework.response import Response
-from rest_framework.generics import ListAPIView, RetrieveAPIView, GenericAPIView
+from rest_framework.generics import ListAPIView
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
-from authentications.views import IsAdmin, IsSuperUser
 from booking.models import Booking
-from booking.serializer import UserBookFlightSerializer
+from booking.serializer import UserBookFlightSerializer, UserGetAllFlightsAvailable
 from flights.models import Flights
 
 # Create your views here.
 
 
 class UserBookingView(ListAPIView):
-    queryset = Booking.objects.all()
-    serializer_class = UserBookFlightSerializer
+    """User booking flight view"""
+
+    serializer_class = UserGetAllFlightsAvailable
     permission_classes = [IsAuthenticated]
 
+    def get_queryset(self):
+        """Getting of all bookings in the system"""
+        return Flights.objects.filter(is_approved=True)
+
     def post(self, request):
-        serializer = self.serializer_class(data=request.data)
-        print(request.data)
+        serializer = UserBookFlightSerializer(data=request.data)
         try:
             if serializer.is_valid():
-                flight_id = serializer.data["flight_id"]
-                user_id = serializer.data["user_id"]
-                seat_number = serializer.data["seat_number"]
-                payment_id = serializer.data["payment_id"]
-                is_booked = serializer.data["is_booked"]
-                type_of_seats = serializer.data["type_of_seats"]
-                flight_object = Flights.objects.filter(id=flight_id)
+                flight_id = serializer.validated_data["flight_id"]
+                user_id = serializer.validated_data["user_id"]
+                seat_number = serializer.validated_data["seat_number"]
+                payment_id = serializer.validated_data["payment_id"]
+                is_booked = serializer.validated_data["is_booked"]
+                type_of_seats = serializer.validated_data["type_of_seats"]
+                flight_object = Flights.objects.filter(id=flight_id.id)
+                flight_get_object = Flights.objects.get(id=flight_id.id)
                 try:
-                    booked_objects = Booking.objects.get(seat_number=seat_number)
-                    return Response(
-                        {"status": "failure", "detail": "seat already booked"}
-                    )
+                    if flight_get_object.seats_available > 0:
+                        booked_objects = Booking.objects.get(seat_number=seat_number)
+                        return Response(
+                            {"status": "failure", "detail": "seat already booked"},
+                            status=status.HTTP_400_BAD_REQUEST,
+                        )
+                    else:
+                        return Response(
+                            {"status": "failure", "detail": "All seats booked"},
+                            status=status.HTTP_400_BAD_REQUEST,
+                        )
                 except Booking.DoesNotExist:
-                    flight_get_object = Flights.objects.get(id=flight_id)
                     if flight_get_object.seats_available > 0:
                         flight_object.update(
                             seats_available=flight_get_object.seats_available - 1
@@ -45,8 +54,8 @@ class UserBookingView(ListAPIView):
                                 "status": "success",
                                 "detail": "booking successful",
                                 "data": {
-                                    "flight_id": flight_id,
-                                    "user_id": user_id,
+                                    "flight": flight_id.flight_name,
+                                    "user": user_id.email_address,
                                     "seat_number": seat_number,
                                     "payment_id": payment_id,
                                     "is_booked": is_booked,
@@ -62,11 +71,11 @@ class UserBookingView(ListAPIView):
                             {"status": "failure", "detail": "All seats booked"},
                             status=status.HTTP_400_BAD_REQUEST,
                         )
-                else:
-                    return Response(
-                        {"status": "failure", "detail": serializer.errors},
-                        status=status.HTTP_400_BAD_REQUEST,
-                    )
+            else:
+                return Response(
+                    {"status": "failure", "detail": serializer.errors},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
         except Flights.DoesNotExist:
             return Response(
                 {"status": "failure", "detail": "flight not found"},
