002fb32d121604200a324da923778d3398fb4d4b
emmanuel-gyateng <57065066+Emmanuel-Okyere@users.noreply.github.com>
2022-08-11 15:33:22 +0000

    Added the type of seat

--- booking/models.py
+++ booking/models.py
@@ -6,12 +6,17 @@ from authentications.models import Users
 class Booking(models.Model):
     """Flight booking model"""
 
+    SEAT_TYPES = (
+        (0, "first class"),
+        (1, "business"),
+        (2, "economy"),
+    )
     flight_id = models.ForeignKey(Flights, on_delete=models.CASCADE)
     user_id = models.ForeignKey(Users, on_delete=models.CASCADE)
     seat_number = models.IntegerField(default=1)
     payment_id = models.IntegerField()
     is_booked = models.BooleanField(default=False)
-    type_of_seats = models.CharField(max_length=120)
+    type_of_seats = models.CharField(max_length=200, null=True, choices=SEAT_TYPES)
 
     def __str__(self):
         return f"{self.flight_id}"
