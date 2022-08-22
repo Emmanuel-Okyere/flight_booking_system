beeab9b6fe306a8600c9f31e0f8a6f38d0896ee4
emmanuel-gyateng <57065066+Emmanuel-Okyere@users.noreply.github.com>
2022-08-22 13:29:30 +0000

    Added Swagger

--- flight/urls.py
+++ flight/urls.py
@@ -16,7 +16,26 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+SchemaView = get_schema_view(
+    openapi.Info(
+        title="Flight Booking System",
+        default_version="v1",
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="gyateng@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
 urlpatterns = [
+    path("", SchemaView.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
     path("admin/", admin.site.urls),
     path("accounts/", include("authentications.urls"), name="authentications"),
     path("flight/", include("flights.urls"), name="flights"),
